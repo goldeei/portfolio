9e89f47cc12d073272e802b713603cf1f5e1ab50
Jake Goldfarb <jake.goldiee@gmail.com>
2025-01-05 21:12:37 -0500

    Add test button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import { Button } from "@/components/button";
+import { Button } from '@/components/button';
 
 export default function Home() {
-	return (
-		<div>
-			<main className="h-svh w-svw place-content-center text-center flex flex-col items-center justify-center">
-				<div className="text-primary">Test Theme Primary</div>
-				<div className="text-secondary">Test Theme Secondary</div>
-				<Button />
-			</main>
-		</div>
-	);
+  return (
+    <div>
+      <main className="h-svh w-svw place-content-center text-center flex flex-col items-center justify-center">
+        <div className="text-primary">Test Theme Primary</div>
+        <div className="text-secondary">Test Theme Secondary</div>
+        <Button>Button</Button>
+      </main>
+    </div>
+  );
 }
