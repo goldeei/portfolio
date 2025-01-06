e6733644b827af9918d62ac29946a3b70e362ccf
Jake Goldfarb <jake.goldiee@gmail.com>
2025-01-05 20:33:23 -0500

    Add some themed colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
 	return (
 		<div>
-			<main>Main</main>
+			<main>
+				<div className="text-primary">Test Theme Primary</div>
+				<div className="text-secondary">Test Theme Secondary</div>
+			</main>
 		</div>
 	);
 }
