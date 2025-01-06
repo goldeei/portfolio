fc0a2654ddb00ddfc4a40d1c0905aeea09d86e85
Jake Goldfarb <jake.goldiee@gmail.com>
2025-01-05 20:43:00 -0500

    Add some test layout, button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/button";
+
 export default function Home() {
 	return (
 		<div>
-			<main>
+			<main className="h-svh w-svw place-content-center text-center flex flex-col items-center justify-center">
 				<div className="text-primary">Test Theme Primary</div>
 				<div className="text-secondary">Test Theme Secondary</div>
+				<Button />
 			</main>
 		</div>
 	);
---
+++ src/components/button.tsx
@@ -0,0 +1,6 @@
+import { Button as SButton } from "./ui/button";
+
+type ButtonProps = {};
+export const Button = ({}: ButtonProps) => {
+	return <SButton />;
+};
