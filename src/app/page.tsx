f7d36031def86f09ce4d3d1e616463f9f8cb55f4
Jake Goldfarb <jake.goldiee@gmail.com>
2025-01-07 21:01:11 -0500

    Setup basic navbar for button styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,25 @@
-import { Button } from '@/components/button';
+import { Button } from "@/components/button";
+import {
+	DownloadSolid,
+	LayersThreeSolid,
+	MoonSolid,
+} from "@mynaui/icons-react";
 
 export default function Home() {
-  return (
-    <div>
-      <main className="h-svh w-svw place-content-center text-center flex flex-col items-center justify-center">
-        <div className="text-primary">Test Theme Primary</div>
-        <div className="text-secondary">Test Theme Secondary</div>
-        <Button>Button</Button>
-      </main>
-    </div>
-  );
+	return (
+		<div className="flex flex-col h-dvh px-36 py-8">
+			<nav className="flex gap-3 justify-end">
+				<Button variant="link">Tech I Use</Button>
+				<Button variant="link">Experiences</Button>
+				<Button variant="link">Projects</Button>
+				<Button icon={<DownloadSolid />}>Resume</Button>
+				<Button icon={<MoonSolid />} />
+				<Button icon={<LayersThreeSolid />} />
+			</nav>
+			<main className="flex-1 place-content-center text-center flex flex-col items-center justify-center">
+				<div className="text-primary">Test Theme Primary</div>
+				<div className="text-secondary">Test Theme Secondary</div>
+			</main>
+		</div>
+	);
 }
