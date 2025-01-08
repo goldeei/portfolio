1c249ce6727acad3203bc202ed035275eac56c91
Jake Goldfarb <jake.goldiee@gmail.com>
2025-01-07 21:01:43 -0500

    Add icon and default variant as outline

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,19 +1,25 @@
+import { JSX } from "react";
+
 import {
-  Button as SButton,
-  ButtonProps as SButtonProps,
-  buttonVariants,
-} from './ui/button';
+	Button as SButton,
+	ButtonProps as SButtonProps,
+	buttonVariants,
+} from "./ui/button";
 
 type ButtonProps = SButtonProps & {
-  disableHover?: boolean;
-  disablePressStyle?: boolean;
+	icon?: JSX.Element;
+	disableHover?: boolean;
+	disablePressStyle?: boolean;
 };
 export const Button = ({ ...props }: ButtonProps) => {
-  const { children } = props;
+	const { children, variant = "outline", className, icon } = props;
 
-  return (
-    <SButton className={buttonVariants({ variant: 'outline' })}>
-      {children}
-    </SButton>
-  );
+	return (
+		<SButton
+			className={buttonVariants({ variant: variant, className: className })}
+		>
+			{icon}
+			{children}
+		</SButton>
+	);
 };
