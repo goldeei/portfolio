b46c07444e99860f37d514a60ae0a14d7f4f94ed
Jake Goldfarb <jgoldfarb@composecure.com>
2025-01-09 12:02:21 -0500

    Add util function to cast shadcn color vars to hex

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,34 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { ClassValue, clsx } from "clsx";
+import convert from "color-convert";
+import { HSL } from "color-convert/conversions";
+import { twMerge } from "tailwind-merge";
 
+/**
+ * Utility function to merge class names using clsx and tailwind-merge.
+ * @param {...ClassValue[]} inputs - The class names to merge.
+ * @returns {string} - The merged class names.
+ */
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
+}
+
+/**
+ * Converts the stored shadcn theme css color variables to a hex value.
+ * @param {string} colorVariableName - The name of the CSS variable containing the HSL value.
+ * @returns {string} - The hex color string.
+ */
+export function hslVarToHex(colorVariableName: string): string {
+	// Get the raw HSL value from the CSS variable
+	const rawHSL = getComputedStyle(document.documentElement)
+		.getPropertyValue(colorVariableName)
+		.trim();
+
+	// Convert the raw HSL string to an array of numbers
+	const hslValueArray = rawHSL
+		.replaceAll("%", "")
+		.split(" ")
+		.map((v) => Number(v)) as HSL;
+
+	// Convert and return the HSL array to a hex color string
+	return convert.hsl.hex(hslValueArray);
 }
