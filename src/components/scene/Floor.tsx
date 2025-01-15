4289f32c28101bc7f834665c8633fc1d34e919f9
Jake Goldfarb <jgoldfarb@composecure.com>
2025-01-15 14:26:40 -0500

    Extract Floor to own component, improve y offset calculation

---
+++ src/components/scene/Floor.tsx
@@ -0,0 +1,20 @@
+import { hslVarToHex } from "@/lib/utils";
+import { Box } from "@react-three/drei";
+
+import { DEFAULT_CUBE_PROPS } from "./constants";
+import { getRoundedBoxFloorY } from "./utils/getRoundedBoxFloorY";
+
+const thickness = 0.15;
+const y = getRoundedBoxFloorY(thickness, DEFAULT_CUBE_PROPS.size[1]);
+
+export const Floor = () => {
+  return (
+    <Box args={[3, thickness, 3]} position={[0, y, 0]} receiveShadow>
+      <meshStandardMaterial
+        roughness={1}
+        metalness={0}
+        color={hslVarToHex("--accent")}
+      />
+    </Box>
+  );
+};
