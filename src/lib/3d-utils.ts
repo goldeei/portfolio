68dd14256af4c7db6e738325283758a0f4be8ae8
Jake Goldfarb <jgoldfarb@composecure.com>
2025-01-16 11:22:41 -0500

    Update to allow alignment with top or bottom

--- src/lib/3d-utils.ts
+++ src/lib/3d-utils.ts
@@ -1,13 +1,26 @@
 /**
- * Calculates the Y position for a bottom cube to align its top face with the bottom face of a top cube.
- * This function is specifically designed for use with the Drei `RoundedBox` component.
+ * Computes the Y position for a cube or face to align its top or bottom with a `Rounded Cube`.
+ * This function is designed to work only with a Drei `RoundedBox` component.
  *
- * The Drei `RoundedBox` component introduces internal offsets due to its rounded corners,
- * which this function accounts for using an empirical adjustment factor.
+ * The Drei `RoundedBox` component introduces internal offsets due to its rounded corners.
+ * This function adjusts for these offsets using an empirical adjustment factor.
  *
- * @param height - The height of the bottom cube.
- * @param boxHeight - The height of the top cube (RoundedBox).
- * @returns The Y position for the bottom cube.
+ * @param boxHeight - The height of the reference cube (RoundedBox).
+ * @param side - Specifies whether the y value should align with the 'top' or 'bottom' side.
+ * @param height - (Optional) The height of the cube to be aligned. Leave undefined for planes.
+ * @returns The Y position for the cube to be aligned.
  */
-export const getRoundedBoxFloorY = (height: number, boxHeight: number) =>
-  (-boxHeight + 2 * 0.065) / 2 - height;
+export const getRoundedBoxMaxY = (
+  boxHeight: number,
+  side: 'top' | 'bottom',
+  height?: number,
+) => {
+  const isTop = side === 'top';
+
+  const h = height ? height : 0;
+  const offset = isTop ? 0 : 0.065;
+
+  const maxY = (-boxHeight + 2 * offset) / 2 - h;
+
+  return isTop ? -maxY : maxY;
+};
