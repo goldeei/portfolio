eb6568b890164ce862f3c59a8183c8d9dfb898ea
Jake Goldfarb <jgoldfarb@composecure.com>
2025-01-19 11:22:35 -0500

    Main cube component as index of cube

---
+++ src/components/scene/cube/index.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+import { CubeMesh } from './cube-mesh';
+import { IntersectGroup } from './intersect-group';
+import { IntersectedFaces } from './types';
+
+export const Cube = () => {
+  const [intersectedFaces, setIntersectedFaces] = useState<IntersectedFaces>({
+    top: false,
+    left: false,
+    right: false,
+  });
+
+  const handleFaceIntersectChange = (intersectedFaces: IntersectedFaces) =>
+    setIntersectedFaces(intersectedFaces);
+
+  return (
+    <group>
+      <IntersectGroup
+        intersectedFaces={intersectedFaces}
+        onFaceIntersect={handleFaceIntersectChange}
+      />
+      <CubeMesh intersectedFaces={intersectedFaces} />
+    </group>
+  );
+};
