e72f95563373a61f59524e94e6d8299bba7f2b1b
Jake Goldfarb <jgoldfarb@composecure.com>
2025-01-19 11:25:09 -0500

    Remove floor and cube from group, use default constants instead

--- src/components/scene/constants.ts
+++ src/components/scene/constants.ts
@@ -1,6 +1,32 @@
-const CUBE_SIZE = Array(3).fill(1) as [number, number, number];
+import { getRoundedBoxMaxY } from '@/lib/3d-utils';
+import { EulerTuple, Vector3Tuple } from 'three';
+import { degToRad } from 'three/src/math/MathUtils.js';
 
-export const DEFAULT_CUBE_PROPS = {
-  size: CUBE_SIZE,
+type DEFAULT_MESH_PROPS = {
+  args: Vector3Tuple;
+  rotation: EulerTuple;
+  position: Vector3Tuple;
+};
+export const DEFAULT_SUBJECT_PROPS: Pick<DEFAULT_MESH_PROPS, 'rotation'> = {
+  rotation: [degToRad(15), degToRad(45), 0],
+};
+
+const cubeSize = Array(3).fill(1) as Vector3Tuple;
+export const DEFAULT_CUBE_PROPS: Omit<DEFAULT_MESH_PROPS, 'position'> & {
+  radius: number;
+} = {
+  args: cubeSize,
   radius: 0.05,
+  rotation: DEFAULT_SUBJECT_PROPS.rotation,
+};
+
+const floorThickness = 0.15;
+export const DEFAULT_FLOOR_PROPS: DEFAULT_MESH_PROPS = {
+  args: [3, 0.15, 3],
+  rotation: DEFAULT_SUBJECT_PROPS.rotation,
+  position: [
+    0,
+    getRoundedBoxMaxY(cubeSize[0], 'bottom', floorThickness) - 0.3,
+    0,
+  ],
 };
--- src/components/scene/index.tsx
+++ src/components/scene/index.tsx
@@ -1,13 +1,12 @@
-import { useR3fState } from "@/context/r3fProvider";
-import { GizmoHelper, GizmoViewport, OrbitControls } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import { useEffect, useRef } from "react";
-import { OrbitControls as ThreeOrbitControls } from "three/examples/jsm/Addons.js";
-import { degToRad } from "three/src/math/MathUtils.js";
+import { useR3fState } from '@/context/r3fProvider';
+import { GizmoHelper, GizmoViewport, OrbitControls } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import { useEffect, useRef } from 'react';
+import { OrbitControls as ThreeOrbitControls } from 'three/examples/jsm/Addons.js';
 
-import { Cube } from "./cube";
-import { Environment } from "./environment";
-import { Floor } from "./floor";
+import { Cube } from './cube';
+import { Environment } from './environment';
+import { Floor } from './floor';
 
 export const Scene = () => {
   const [state] = useR3fState();
@@ -23,10 +22,8 @@ export const Scene = () => {
   return (
     <Canvas orthographic camera={{ zoom: 50, position: [0, 0, 100] }} shadows>
       <Environment />
-      <group rotation={[degToRad(15), degToRad(45), 0]} position={[0, 0, 0]}>
-        <Cube />
-        <Floor />
-      </group>
+      <Cube />
+      <Floor />
       {state.isOrbitControlEnabled && <OrbitControls ref={orbitControlsRef} />}
       <GizmoHelper
         alignment="bottom-right" // widget alignment within scene
