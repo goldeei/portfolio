d1f85c831d47aa0903af000af7c169fba0e010c8
Jake Goldfarb <jgoldfarb@composecure.com>
2025-01-19 11:21:51 -0500

    Extract cube intersect detect mesh

---
+++ src/components/scene/cube/intersect-group.tsx
@@ -0,0 +1,61 @@
+import { getRoundedBoxMaxY } from '@/lib/3d-utils';
+import { useCallback, useMemo } from 'react';
+import { degToRad } from 'three/src/math/MathUtils.js';
+
+import { DEFAULT_CUBE_PROPS } from '../constants';
+import { PlaneIntersect } from '../plane-intersect';
+import { Face, IntersectedFaces } from './types';
+
+interface IntersectGroupProps {
+  intersectedFaces: IntersectedFaces;
+  onFaceIntersect: (intersectedFaces: IntersectedFaces) => void;
+}
+
+export const IntersectGroup = ({ ...props }: IntersectGroupProps) => {
+  const { intersectedFaces, onFaceIntersect } = props;
+  const { rotation, args, radius } = DEFAULT_CUBE_PROPS;
+
+  const cornerOffset = useMemo(
+    () => (args[0] * Math.sqrt(3)) / 3 - radius,
+    [args, radius],
+  );
+
+  const handleIntersect = useCallback(
+    (name: string, isIntersected: boolean) => {
+      const face = name as Face;
+      if (intersectedFaces[face] !== isIntersected) {
+        return onFaceIntersect({
+          ...intersectedFaces,
+          [face]: isIntersected,
+        });
+      }
+    },
+    [intersectedFaces, onFaceIntersect],
+  );
+
+  return (
+    <group>
+      <PlaneIntersect
+        name="left"
+        onIntersectChange={handleIntersect}
+        size={[args[0], args[1]]}
+        position={[-cornerOffset, 0, 0]}
+        rotation={[rotation[0], 0, 0]}
+      />
+      <PlaneIntersect
+        name="right"
+        onIntersectChange={handleIntersect}
+        size={[args[0], args[1]]}
+        position={[cornerOffset, 0, 0]}
+        rotation={[rotation[0], 0, 0]}
+      />
+      <PlaneIntersect
+        name="top"
+        onIntersectChange={handleIntersect}
+        rotation={[-degToRad(90) + rotation[0], 0, -rotation[1]]}
+        size={[args[0], args[0]]}
+        position={[0, getRoundedBoxMaxY(args[1], 'top', 0), 0.15]}
+      />
+    </group>
+  );
+};
