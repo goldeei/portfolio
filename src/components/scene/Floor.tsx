752f4943b1a96bd27e1d4fb4fc5cc9ba42da5af5
Jake Goldfarb <jgoldfarb@composecure.com>
2025-01-19 11:26:03 -0500

    Add DEFAULT_FLOOR_PROPS

--- src/components/scene/Floor.tsx
+++ src/components/scene/Floor.tsx
@@ -1,15 +1,11 @@
-import { getRoundedBoxMaxY } from "@/lib/3d-utils";
-import { hslVarToHex } from "@/lib/utils";
-import { Box } from "@react-three/drei";
+import { hslVarToHex } from '@/lib/utils';
+import { Box } from '@react-three/drei';
 
-import { DEFAULT_CUBE_PROPS } from "./constants";
-
-const thickness = 0.15;
-const y = getRoundedBoxMaxY(DEFAULT_CUBE_PROPS.size[1], 'bottom', thickness);
+import { DEFAULT_FLOOR_PROPS } from './constants';
 
 export const Floor = () => {
   return (
-    <Box args={[3, thickness, 3]} position={[0, y, 0]} receiveShadow>
+    <Box {...DEFAULT_FLOOR_PROPS} receiveShadow>
       <meshStandardMaterial
         roughness={1}
         metalness={0}
