c6325b617d1ccc3a154f4600206de2a7da9d1b19
Jake Goldfarb <jgoldfarb@composecure.com>
2025-01-24 10:50:00 -0500

    Add hook for extruding geometry

---
+++ src/hooks/useSvgAsExtrudeGeometry.ts
@@ -0,0 +1,36 @@
+import { useLoader } from '@react-three/fiber';
+import { useMemo } from 'react';
+import { ExtrudeGeometry, Shape } from 'three';
+import { SVGLoader } from 'three/examples/jsm/Addons.js';
+
+export const useSvgAsExtrudeGeometry = (
+  svgUrl: string,
+  depth: number,
+): ExtrudeGeometry => {
+  const data = useLoader(SVGLoader, svgUrl);
+
+  const geometry = useMemo(() => {
+    const shapes: Shape[] = [];
+    data.paths.forEach((p) => {
+      p.toShapes(false).forEach((sp) => {
+        shapes.push(sp);
+      });
+    });
+
+    const geo = new ExtrudeGeometry(shapes, {
+      steps: 2,
+      depth: depth,
+      bevelEnabled: true,
+      bevelThickness: 0.25,
+      bevelSize: 1,
+      bevelOffset: 0,
+      bevelSegments: 2,
+    });
+
+    geo.center();
+    geo.scale(0.1, 0.1, 0.1);
+    return geo;
+  }, [data]);
+
+  return geometry;
+};
