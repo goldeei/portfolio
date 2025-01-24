7481b6b5980f3fc8d4f51ae952f80975dee89bdd
Jake Goldfarb <jgoldfarb@composecure.com>
2025-01-24 10:47:49 -0500

    Add emissive animation

--- src/animations/r3f/showCubeFace.ts
+++ src/animations/r3f/showCubeFace.ts
@@ -2,13 +2,19 @@ import { SpringRef } from 'react-spring';
 import { EulerTuple } from 'three';
 
 export const showCubeFace = (
-  api: SpringRef<{ rotation: EulerTuple }>,
+  api: SpringRef<{
+    rotation: EulerTuple;
+    lightIntensity: number;
+    emissiveIntensity: number;
+  }>,
   rotation: EulerTuple,
 ) => {
   api.start({
     to: async (next) => {
       await next({
         rotation: rotation,
+        lightIntensity: 20,
+        emissiveIntensity: 10,
         config: {},
       });
     },
