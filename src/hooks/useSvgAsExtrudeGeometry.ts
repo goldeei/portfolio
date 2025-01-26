a86d31700304124dc368f80a88477a71b6b214f7
Jake Goldfarb <jake.goldiee@gmail.com>
2025-01-26 17:35:44 -0500

    Resolve not working properly with svgs

--- src/hooks/useSvgAsExtrudeGeometry.ts
+++ src/hooks/useSvgAsExtrudeGeometry.ts
@@ -1,36 +1,26 @@
 import { useLoader } from '@react-three/fiber';
 import { useMemo } from 'react';
-import { ExtrudeGeometry, Shape } from 'three';
+import { BufferGeometry, ExtrudeGeometry, Shape, ShapeGeometry } from 'three';
 import { SVGLoader } from 'three/examples/jsm/Addons.js';
 
 export const useSvgAsExtrudeGeometry = (
   svgUrl: string,
   depth: number,
+  toShapes: boolean,
 ): ExtrudeGeometry => {
   const data = useLoader(SVGLoader, svgUrl);
 
   const geometry = useMemo(() => {
-    const shapes: Shape[] = [];
-    data.paths.forEach((p) => {
-      p.toShapes(false).forEach((sp) => {
-        shapes.push(sp);
-      });
+    const shapes = data.paths.flatMap((path) => path.toShapes(toShapes));
+    const geometry = new ExtrudeGeometry(shapes, {
+      steps: 2,
+      depth,
     });
 
-    const geo = new ExtrudeGeometry(shapes, {
-      steps: 4,
-      depth: depth * 2,
-      bevelEnabled: true,
-      // bevelThickness: 0.25,
-      // bevelSize: 1,
-      // bevelOffset: 0,
-      // bevelSegments: 2,
-    });
-
-    geo.center();
-    geo.scale(0.1, 0.1, 0.1);
-    return geo;
-  }, [data.paths, depth]);
+    geometry.center();
+    geometry.scale(0.1, 0.1, 0.1);
+    return geometry;
+  }, [data.paths, depth, toShapes]);
 
   return geometry;
 };
