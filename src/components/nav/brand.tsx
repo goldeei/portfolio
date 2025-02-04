483b8a9b277b9f9c2628270625df0663a4bd3589
Jake Goldfarb <jgoldfarb@composecure.com>
2025-02-04 11:52:08 -0500

    Extract brand

---
+++ src/components/nav/brand.tsx
@@ -0,0 +1,14 @@
+import { Logo } from '@/assets/icons';
+
+import { Button } from '../button';
+
+export const Brand = () => (
+  <div className="relative flex size-fit items-center justify-center">
+    <Button
+      variant="link"
+      className="h-16 border-none p-0 shadow-inner transition-transform hover:-translate-y-0.5 [&_svg]:size-full"
+    >
+      <Logo />
+    </Button>
+  </div>
+);
