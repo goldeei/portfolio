57c67eaf00ae8f7f693ad25783cf4d27ac3433de
Jake Goldfarb <jgoldfarb@composecure.com>
2025-02-05 15:44:13 -0500

    Use var for navbar height

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,17 +1,19 @@
-import { CubeIcon } from '@/assets/icons';
-import { Button } from '@/components/button';
-import { Switch } from '@/components/switch';
-import { useR3fState } from '@/context/r3fProvider';
-import { DownloadSolid } from '@mynaui/icons-react';
+'use client';
 
-import { Brand } from './brand';
+import { CubeIcon } from "@/assets/icons";
+import { Button } from "@/components/button";
+import { Switch } from "@/components/switch";
+import { useR3fState } from "@/context/r3fProvider";
+import { DownloadSolid } from "@mynaui/icons-react";
+
+import { Brand } from "./brand";
 
 export const Nav = () => {
   const [r3fState, updateR3fState] = useR3fState();
   const { isCanvasOnTop, isOrbitControlEnabled } = r3fState;
 
   return (
-    <nav className="z-50 flex items-center justify-between">
+    <nav className="z-50 flex h-[var(--navbar-height)] items-center justify-between">
       <Brand />
       <div className="flex gap-3">
         <Button variant="link">Tech I Use</Button>
