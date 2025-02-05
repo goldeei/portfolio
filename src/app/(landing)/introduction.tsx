26db05bf1f390e46483db22aff16f534be86d53e
Jake Goldfarb <jgoldfarb@composecure.com>
2025-02-05 15:43:52 -0500

    Add introduction text and layout

--- src/app/(landing)/introduction.tsx
+++ src/app/(landing)/introduction.tsx
@@ -1,3 +1,18 @@
 export const Introduction = () => {
-  return <div>Hi</div>;
+  return (
+    <div className="h-full-content flex flex-col justify-center">
+      <h1 className="text-4xl font-medium text-primary">Jake Goldfarb</h1>
+      <h2 className="text-2xl font-medium text-secondary">UI/UX Engineer</h2>
+      {/* TODO change this text */}
+      <p className="mt-3 w-[500px] text-secondary">
+        I’m a{' '}
+        <span className="font-bold text-primary">
+          web application developer with 5+ years
+        </span>{' '}
+        specializing in creating dashboards for presenting, interacting with,
+        and managing complex data. I like exploring unique ways to make data
+        digestible and experimenting with new web design methods.
+      </p>
+    </div>
+  );
 };
