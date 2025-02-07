aefe77b5a3d0f304829e4fc9af4918f41bd7960d
Jake Goldfarb <jgoldfarb@composecure.com>
2025-02-07 12:17:38 -0500

    Pass ref

--- src/components/landing-spot/diamond.tsx
+++ src/components/landing-spot/diamond.tsx
@@ -1,11 +1,16 @@
+import { Ref } from 'react';
+
 export const Diamond = ({
   className,
   style,
+  ref,
 }: {
   className: string;
   style: React.CSSProperties;
+  ref?: Ref<HTMLDivElement>;
 }) => (
   <div
+    ref={ref}
     style={{
       clipPath: 'polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%)',
       ...style,
