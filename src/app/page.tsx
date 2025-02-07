043c8371ba2554c898c2aecb5c51718881e21fc9
Jake Goldfarb <jgoldfarb@composecure.com>
2025-02-07 12:24:01 -0500

    Move scene wrapper div to Scene

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,14 @@
 'use client';
 
-import { Scene } from "@/components/scene";
-import { useR3fState } from "@/context/r3fProvider";
-import clsx from "clsx";
+import { Scene } from '@/components/scene';
 
-import Landing from "./(landing)/page";
+import Landing from './(landing)/page';
 
 export default function Home() {
-  const [r3fState] = useR3fState();
-
   return (
     <main>
       <Landing />
-      <div
-        className={clsx(
-          'absolute inset-0 h-screen w-full',
-          r3fState.isCanvasOnTop ? 'z-20' : 'z-0',
-        )}
-      >
-        <Scene />
-      </div>
+      <Scene />
     </main>
   );
 }
--- src/components/scene/index.tsx
+++ src/components/scene/index.tsx
@@ -1,12 +1,12 @@
-import { useR3fState } from "@/context/r3fProvider";
-import { GizmoHelper, GizmoViewport, OrbitControls } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import { useEffect, useRef } from "react";
-import { OrbitControls as ThreeOrbitControls } from "three/examples/jsm/Addons.js";
+import { useR3fState } from '@/context/r3fProvider';
+import { OrbitControls } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import clsx from 'clsx';
+import { useEffect, useRef } from 'react';
+import { OrbitControls as ThreeOrbitControls } from 'three/examples/jsm/Addons.js';
 
-import { Cube } from "./cube";
-import { Environment } from "./environment";
-import { Floor } from "./floor";
+import { Cube } from './cube';
+import { Environment } from './environment';
 
 export const Scene = () => {
   const [state] = useR3fState();
@@ -20,12 +20,20 @@ export const Scene = () => {
   }, [state.isOrbitControlEnabled]);
 
   return (
-    <Canvas orthographic camera={{ zoom: 50, position: [0, 0, 100] }} shadows>
-      <Environment />
-      <Cube />
-      {/* <Floor /> */}
-      {state.isOrbitControlEnabled && <OrbitControls ref={orbitControlsRef} />}
-      <GizmoHelper
+    <div
+      className={clsx(
+        'absolute inset-0 size-32 border',
+        state.isCanvasOnTop ? 'z-20' : 'z-0',
+      )}
+    >
+      <Canvas orthographic camera={{ zoom: 50, position: [0, 0, 20] }} shadows>
+        <Environment />
+        <Cube />
+        {/* <Floor /> */}
+        {state.isOrbitControlEnabled && (
+          <OrbitControls ref={orbitControlsRef} />
+        )}
+        {/* <GizmoHelper
         alignment="bottom-right" // widget alignment within scene
         margin={[80, 80]} // widget margins (X, Y)
       >
@@ -33,7 +41,8 @@ export const Scene = () => {
           axisColors={['red', 'green', 'blue']}
           labelColor="black"
         />
-      </GizmoHelper>
-    </Canvas>
+      </GizmoHelper> */}
+      </Canvas>
+    </div>
   );
 };
