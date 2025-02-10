f14557e053eeb1b12845f8d582fa2aba33680263
Jake Goldfarb <jgoldfarb@composecure.com>
2025-02-10 13:58:34 -0500

    Add a landing spot to navbar for initial

--- src/components/nav/brand.tsx
+++ src/components/nav/brand.tsx
@@ -1,12 +1,21 @@
 import { Logo } from '@/assets/icons';
+import { LandingPosition } from '@/types/landingPosition';
 
 import { Button } from '../button';
+import { LandingSpot } from '../landing-spot';
 
 export const Brand = () => (
   <div className="relative flex size-fit items-center justify-center">
+    <LandingSpot
+      className="top-12 -z-50"
+      position="absolute"
+      name={LandingPosition.Initial}
+      left={-40}
+      isVisible={true}
+    />
     <Button
       variant="link"
-      className="size-16 border-none p-0 shadow-inner transition-transform hover:-translate-y-0.5 [&_svg]:size-full"
+      className="z-10 size-16 border-none p-0 shadow-inner transition-transform hover:-translate-y-0.5 [&_svg]:size-full"
     >
       <Logo />
     </Button>
--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,19 +1,19 @@
 'use client';
 
-import { CubeIcon } from "@/assets/icons";
-import { Button } from "@/components/button";
-import { Switch } from "@/components/switch";
-import { useR3fState } from "@/context/r3fProvider";
-import { DownloadSolid } from "@mynaui/icons-react";
+import { CubeIcon } from '@/assets/icons';
+import { Button } from '@/components/button';
+import { Switch } from '@/components/switch';
+import { useR3fState } from '@/context/r3fProvider';
+import { DownloadSolid } from '@mynaui/icons-react';
 
-import { Brand } from "./brand";
+import { Brand } from './brand';
 
 export const Nav = () => {
   const [r3fState, updateR3fState] = useR3fState();
   const { isCanvasOnTop, isOrbitControlEnabled } = r3fState;
 
   return (
-    <nav className="z-50 flex h-[var(--navbar-height)] items-center justify-between">
+    <nav className="flex h-[var(--navbar-height)] items-center justify-between">
       <Brand />
       <div className="flex gap-3">
         <Button variant="link">Tech I Use</Button>
