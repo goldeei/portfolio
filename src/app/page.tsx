71da848abca3a19be7347538d4259c621e6a7c1d
Jake Goldfarb <jgoldfarb@composecure.com>
2025-02-10 13:59:04 -0500

    Test moving cube position onload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
 'use client';
 
 import { Scene } from '@/components/scene';
+import { useCubeState } from '@/context/cubeStateProvider';
+import { LandingPosition } from '@/types/landingPosition';
+import { useEffect } from 'react';
 
 import Landing from './(landing)/page';
 
 export default function Home() {
+  const [_, setCubeState] = useCubeState();
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setCubeState({
+        type: 'SET_POSITION',
+        payload: LandingPosition.Introduction,
+      });
+    }, 1000);
+
+    return () => clearTimeout(timeout);
+  }, [setCubeState]);
+
   return (
     <main>
       <Landing />
