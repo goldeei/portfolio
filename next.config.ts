6e11df3da4fe8aeb2ca5f223c4715d833a58a3ce
Jake Goldfarb <jake.goldiee@gmail.com>
2025-02-15 11:15:34 -0500

    Remove turbopack flag on run dev, add some robustness to svgr loader

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,18 @@ const nextConfig: NextConfig = {
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ['@svgr/webpack'],
+      issuer: /\.[jt]sx?$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            typescript: true,
+            dimensions: false,
+          },
+        },
+      ],
     });
+
     return config;
   },
 };
