187463c8cefe8317abf7b3b9fab8e5a9368228a6
Jake Goldfarb <jgoldfarb@composecure.com>
2025-02-18 15:11:15 -0500

    Test some new animations

--- src/components/tabs/tabs.tsx
+++ src/components/tabs/tabs.tsx
@@ -1,5 +1,6 @@
 import { cn } from '@/lib/style-utils';
-import { useMemo, useState } from 'react';
+import { motion } from 'motion/react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { Label } from '../ui/label';
 import { RadioGroup, RadioGroupItem } from '../ui/radio-group';
 
@@ -20,7 +21,7 @@ const radioClasses = 'h-full min-h-8 cursor-pointer transition-all';
 
 export const Tabs = ({ ...props }: TabGroupProps) => {
   const { tabs, defaultValue, onValueChange } = props;
-  const [selectedTab, setSelectedTab] = useState(defaultValue || '');
+  const [selectedTab, setSelectedTab] = useState(defaultValue);
   const handleChange = (v: string) => {
     setSelectedTab(v);
     if (onValueChange) {
@@ -28,50 +29,104 @@ export const Tabs = ({ ...props }: TabGroupProps) => {
     }
   };
 
+  const clipRef = useRef<HTMLDivElement>(null);
+  const indicatorRef = useRef<HTMLDivElement>(null);
+
   const content = useMemo(
     () => tabs.find((tab) => tab.value === selectedTab)?.content,
     [selectedTab, tabs],
   );
 
+  const [indicatorClip, setIndicatorClip] = useState<string | undefined>(
+    undefined,
+  );
+  const [barPosition, setBarPosition] = useState<
+    { right: number; top: number } | undefined
+  >(undefined);
+  useEffect(() => {
+    if (clipRef.current && indicatorRef.current) {
+      const clipRect = clipRef.current.getBoundingClientRect();
+      const indicatorRect = indicatorRef.current.getBoundingClientRect();
+
+      console.log(clipRect.width, indicatorRect.width);
+
+      const widthDiff = indicatorRect.width - clipRect.width;
+      const sideOffset = widthDiff / 2;
+
+      const clip = `inset(0 ${sideOffset}px 0 ${sideOffset}px)`;
+
+      setBarPosition({
+        right: sideOffset,
+        top: 0,
+      });
+
+      setIndicatorClip(clip);
+    }
+  }, []);
+
   return (
     <div className="flex h-96 gap-12">
-      <RadioGroup
-        className="relative flex h-full w-fit flex-col justify-between gap-0"
-        onValueChange={handleChange}
-        value={selectedTab}
-      >
-        {tabs.map(({ value, label }) => (
-          <div
-            key={value}
-            className="flex items-center justify-between gap-4 py-4"
-          >
-            <Label
-              key={`${value}-label`}
-              className={cn(
-                'ps-1 text-secondary/50',
-
-                value === selectedTab
-                  ? 'text-secondary'
-                  : 'hover:text-secondary/75',
-              )}
+      <nav>
+        <ul className="relative flex h-full w-fit flex-col justify-between gap-0">
+          {tabs.map(({ value, label }) => (
+            <motion.li
+              key={value}
+              initial={false}
+              className="flex cursor-pointer items-center justify-between gap-4 py-4 [&_div]:rounded-full"
+              onClick={() => setSelectedTab(value)}
+              whileHover={{ color: '#fff' }}
             >
-              {label}
-            </Label>
-            <RadioGroupItem
-              value={value}
-              id={value}
-              className={cn(
-                'size-5 border-none bg-accent shadow-inner outline-none ring-0 [&_svg]:hidden',
-
-                value === selectedTab
-                  ? 'bg-secondary shadow-sm'
-                  : 'hover:bg-secondary/50',
-              )}
-            />
-          </div>
-        ))}
-        <div className="absolute right-2 -z-50 h-full w-1.5 rounded bg-accent" />
-      </RadioGroup>
+              <motion.div
+                initial={false}
+                className={cn('ps-1')}
+                animate={{
+                  color:
+                    value === selectedTab
+                      ? 'text-secondary'
+                      : 'text-secondary/50',
+                }}
+              >
+                {label}
+              </motion.div>
+              <div
+                className={cn(
+                  'flex size-6 items-center justify-center bg-accent shadow-inner transition-colors',
+                  value === selectedTab && '',
+                )}
+              >
+                {value === selectedTab && (
+                  <motion.div
+                    ref={indicatorRef}
+                    className={cn(
+                      'size-6 border-2 border-accent bg-secondary shadow-inner',
+                    )}
+                    style={{ clipPath: 'inset(0 0 0 0)' }}
+                    layoutId="active-indicator"
+                    id="active-indicator"
+                    animate={{
+                      clipPath: [
+                        'inset(0 0 0 0)',
+                        indicatorClip,
+                        indicatorClip,
+                        'inset(0 0 0 0)',
+                      ],
+                    }}
+                    transition={{
+                      duration: 0.3,
+                      times: [0, 0.15, 0.85, 1],
+                    }}
+                  />
+                )}
+              </div>
+            </motion.li>
+          ))}
+          <div
+            ref={clipRef}
+            className="absolute -z-50 h-full w-1.5 rounded bg-accent"
+            style={barPosition}
+          />
+        </ul>
+      </nav>
       <div>{content}</div>
     </div>
   );
