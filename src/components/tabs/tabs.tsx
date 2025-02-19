258946ae11d52f7288d9e0afd642219401a1c20b
Jake Goldfarb <jgoldfarb@composecure.com>
2025-02-19 09:15:09 -0500

    Remove active indicator, cleanup unused state/ref

--- src/components/tabs/tabs.tsx
+++ src/components/tabs/tabs.tsx
@@ -1,8 +1,8 @@
-import { cn, hslVarToHex } from '@/lib/style-utils';
+import { cn } from '@/lib/style-utils';
 import { motion } from 'motion/react';
 import { useEffect, useMemo, useRef, useState } from 'react';
-import { Label } from '../ui/label';
-import { RadioGroup, RadioGroupItem } from '../ui/radio-group';
+
+import { ActiveIndicator } from './active-indicator';
 
 interface TabGroupProps {
   tabs: {
@@ -11,13 +11,12 @@ interface TabGroupProps {
     content: React.ReactNode;
   }[];
   onValueChange?: (value: string) => void;
-  defaultValue?: string;
+  defaultValue: string;
 }
 
-const gridClasses = (tabCount: number) =>
-  `grid-rows-[repeat(${tabCount}, auto)] grid items-center py-2 gap-3 h-full`;
-
-const radioClasses = 'h-full min-h-8 cursor-pointer transition-all';
+const indicatorWidth = 24;
+const trackWidthOffset = 0.3;
+const trackWidth = indicatorWidth * trackWidthOffset;
 
 export const Tabs = ({ ...props }: TabGroupProps) => {
   const { tabs, defaultValue, onValueChange } = props;
@@ -29,40 +28,10 @@ export const Tabs = ({ ...props }: TabGroupProps) => {
     }
   };
 
-  const clipRef = useRef<HTMLDivElement>(null);
-  const indicatorRef = useRef<HTMLDivElement>(null);
-
   const content = useMemo(
     () => tabs.find((tab) => tab.value === selectedTab)?.content,
     [selectedTab, tabs],
   );
-
-  const [indicatorClip, setIndicatorClip] = useState<string | undefined>(
-    undefined,
-  );
-  const [barPosition, setBarPosition] = useState<
-    { right: number; top: number } | undefined
-  >(undefined);
-  useEffect(() => {
-    if (clipRef.current && indicatorRef.current) {
-      const clipRect = clipRef.current.getBoundingClientRect();
-      const indicatorRect = indicatorRef.current.getBoundingClientRect();
-
-      console.log(clipRect.width, indicatorRect.width);
-
-      const widthDiff = indicatorRect.width - clipRect.width;
-      const sideOffset = widthDiff / 2;
-
-      const clip = `inset(0 ${sideOffset}px 0 ${sideOffset}px)`;
-
-      setBarPosition({
-        right: sideOffset,
-        top: 0,
-      });
-
-      setIndicatorClip(clip);
-    }
-  }, []);
 
   return (
     <div className="flex h-96 gap-12">
@@ -72,13 +41,12 @@ export const Tabs = ({ ...props }: TabGroupProps) => {
             <motion.li
               key={value}
               initial={false}
-              className="flex cursor-pointer items-center justify-between gap-4 py-4 [&_div]:rounded-full"
+              className="flex cursor-pointer items-center justify-between gap-4 py-4"
               onClick={() => setSelectedTab(value)}
               whileHover={{ color: '#fff' }}
             >
               <motion.div
                 initial={false}
-                className={cn('ps-1')}
                 animate={{
                   color:
                     value === selectedTab
@@ -88,51 +56,20 @@ export const Tabs = ({ ...props }: TabGroupProps) => {
               >
                 {label}
               </motion.div>
-              <div
-                className={cn(
-                  'flex size-6 items-center justify-center bg-accent shadow-inner transition-colors',
-                  value === selectedTab && '',
-                )}
-              >
-                {value === selectedTab && (
-                  <motion.div
-                    ref={indicatorRef}
-                    layoutId="active-indicator"
-                    id="active-indicator"
-                    transition={{
-                      duration: 0.75,
-                      ease: 'anticipate', // Easing function
-                    }}
-                  >
-                    <motion.svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width={'100%'}
-                      height={'100%'}
-                      viewBox="0 0 100 100"
-                      fill={hslVarToHex('--secondary')}
-                    >
-                      <motion.ellipse
-                        cx="50"
-                        cy="50"
-                        ry="40"
-                        animate={{
-                          rx: [40, 15, 40], // Keyframes for rx
-                        }}
-                        transition={{
-                          duration: 0.75, // Duration of the animation
-                          ease: 'anticipate', // Easing function
-                        }}
-                      />
-                    </motion.svg>
-                  </motion.div>
-                )}
-              </div>
+              <ActiveIndicator
+                width={indicatorWidth}
+                trackWidthOffset={trackWidthOffset}
+                value={value}
+                selectedTab={selectedTab}
+              />
             </motion.li>
           ))}
           <div
-            ref={clipRef}
-            className="absolute -z-50 h-full w-1.5 rounded bg-accent"
-            style={barPosition}
+            className="absolute -z-50 h-full rounded-full bg-accent"
+            style={{
+              width: trackWidth,
+              right: (indicatorWidth - trackWidth) / 2,
+            }}
           />
         </ul>
       </nav>
