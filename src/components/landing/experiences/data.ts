ca0d18b8d727313a2a87424834682cda5984bf33
Jake Goldfarb <jgoldfarb@composecure.com>
2025-02-24 11:48:17 -0500

    Add some filler experience data

---
+++ src/components/landing/experiences/data.ts
@@ -0,0 +1,30 @@
+import { ExperienceProps } from './types';
+
+const composecure: ExperienceProps = {
+  startDate: new Date('04/23/2023'),
+  endDate: 'Present',
+  summary:
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium itaque doloribus repellendus! Aperiam placeat aspernatur cum quam, enim obcaecati, corrupti impedit doloribus exercitationem dignissimos reprehenderit laudantium, quod molestias assumenda! Ipsam.',
+  achievements: ['achievement 1', 'achievement 2', 'achievement 3'],
+  projects: ['project 1', 'project 2', 'project 3'],
+};
+
+const dcs: ExperienceProps = {
+  startDate: new Date('05/19/2019'),
+  endDate: new Date('04/23/2023'),
+  summary:
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium itaque doloribus repellendus! Aperiam placeat aspernatur cum quam, enim obcaecati, corrupti impedit doloribus exercitationem dignissimos reprehenderit laudantium, quod molestias assumenda! Ipsam.',
+  achievements: ['achievement 1', 'achievement 2', 'achievement 3'],
+  projects: ['project 1', 'project 2', 'project 3'],
+};
+
+const oit: ExperienceProps = {
+  startDate: new Date('01/03/2018'),
+  endDate: new Date('05/12/2019'),
+  summary:
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium itaque doloribus repellendus! Aperiam placeat aspernatur cum quam, enim obcaecati, corrupti impedit doloribus exercitationem dignissimos reprehenderit laudantium, quod molestias assumenda! Ipsam.',
+  achievements: ['achievement 1', 'achievement 2', 'achievement 3'],
+  projects: ['project 1', 'project 2', 'project 3'],
+};
+
+export { composecure, dcs, oit };
