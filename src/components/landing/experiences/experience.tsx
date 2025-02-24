03777984b3971c7d1f3f8c50e2076488d4bdd095
Jake Goldfarb <jgoldfarb@composecure.com>
2025-02-24 12:08:01 -0500

    Remove test elements

--- src/components/landing/experiences/experience.tsx
+++ src/components/landing/experiences/experience.tsx
@@ -32,22 +32,6 @@ export const Experience = ({ ...props }: ExperienceProps) => {
           ))}
         </ul>
       </div>
-      <div>
-        <h4 className="text-xl font-medium">Achievements</h4>
-        <ul>
-          {achievements.map((a) => (
-            <li key={a}>- {a}</li>
-          ))}
-        </ul>
-      </div>
-      <div>
-        <h4 className="text-xl font-medium">Projects</h4>
-        <ul>
-          {projects.map((p) => (
-            <li key={p}>- {p}</li>
-          ))}
-        </ul>
-      </div>
     </div>
   );
 };
