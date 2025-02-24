73c7b575f9d2bffc4472f36f0210e90c4f833533
Jake Goldfarb <jgoldfarb@composecure.com>
2025-02-24 11:54:49 -0500

    Add some variation

--- src/components/landing/experiences/data.ts
+++ src/components/landing/experiences/data.ts
@@ -22,7 +22,7 @@ const oit: ExperienceProps = {
   startDate: new Date('01/03/2018'),
   endDate: new Date('05/12/2019'),
   summary:
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium itaque doloribus repellendus! Aperiam placeat aspernatur cum quam, enim obcaecati, corrupti impedit doloribus exercitationem dignissimos reprehenderit laudantium, quod molestias assumenda! Ipsam.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium itaque doloribus repellendus! Aperiam placeat aspernatur cum quam, enim obcaecati, corrupti impedit doloribus exercitationem dignissimos reprehenderit laudantium, quod molestias assumenda! Ipsam. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium itaque doloribus repellendus! Aperiam placeat aspernatur cum quam, enim obcaecati, corrupti impedit doloribus exercitationem dignissimos reprehenderit laudantium, quod molestias assumenda! Ipsam.',
   achievements: ['achievement 1', 'achievement 2', 'achievement 3'],
   projects: ['project 1', 'project 2', 'project 3'],
 };
