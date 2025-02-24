644ec7b8552eb127c5ce95735637bc1c077810fb
Jake Goldfarb <jgoldfarb@composecure.com>
2025-02-24 10:14:07 -0500

    Update tabs values

--- src/components/landing/introduction.tsx
+++ src/components/landing/introduction.tsx
@@ -21,15 +21,23 @@ export const Introduction = () => {
         digestible and experimenting with new web design methods.
       </p>
       <Tabs
-        defaultValue="tab-1"
+        defaultTab={0}
         tabs={[
           {
             label: 'Tab1sdsad',
             value: 'tab-1',
-            content: <div>Tab 1 Content</div>,
+            content: { header: 'Tab 1 Header', body: <div>Tab 1 Content</div> },
           },
-          { label: 'Tab 2', value: 'tab-2', content: <div>Tab 2 Content</div> },
-          { label: 'Tab 3', value: 'tab-3', content: <div>Tab 3 Content</div> },
+          {
+            label: 'Tab 2',
+            value: 'tab-2',
+            content: { header: 'Tab 2 Header', body: <div>Tab 2 Content</div> },
+          },
+          {
+            label: 'Tab 3',
+            value: 'tab-3',
+            content: { header: 'Tab 3 Header', body: <div>Tab 3 Content</div> },
+          },
         ]}
       />
     </div>
