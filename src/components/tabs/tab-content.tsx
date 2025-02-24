6e096914b68a9f5b7a3d9f5204a1050babf446c2
Jake Goldfarb <jgoldfarb@composecure.com>
2025-02-24 10:13:58 -0500

    Add base animation

--- src/components/tabs/tab-content.tsx
+++ src/components/tabs/tab-content.tsx
@@ -1,10 +1,27 @@
-import { TabsContent as STabsContent } from "../ui/tabs";
+import { AnimatePresence, motion, Transition } from 'motion/react';
 
 interface TabContentProps {
   value: string;
+  header: string;
+  children: string | React.ReactNode;
+  transitionProps: Transition;
 }
 export const TabContent = ({ ...props }: TabContentProps) => {
-  const { value } = props;
+  const { value, header, children, transitionProps } = props;
 
-  return <STabsContent value={value}>Tab Content-{value}</STabsContent>;
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={value}
+        initial={{ y: 10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: -10, opacity: 0 }}
+        className="text-secondary"
+        transition={transitionProps}
+      >
+        <div className="mb-4 text-lg font-medium">{header}</div>
+        <div>{children}</div>
+      </motion.div>
+    </AnimatePresence>
+  );
 };
