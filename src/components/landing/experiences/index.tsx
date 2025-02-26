9aa82b2ba4db851e55e02921f88700fbf2f0c82e
Jake Goldfarb <jgoldfarb@composecure.com>
2025-02-26 13:49:45 -0500

    Set height on content

--- src/components/landing/experiences/index.tsx
+++ src/components/landing/experiences/index.tsx
@@ -6,38 +6,40 @@ export const Experiences = () => {
   return (
     <div className="flex h-screen flex-col justify-center gap-6">
       <h1 className="text-primary text-4xl font-medium">Experiences</h1>
-      <Tabs
-        defaultTab={0}
-        tabs={[
-          {
-            label: 'composecure',
-            subLabel: composecure.date,
-            value: 'composecure',
-            content: {
-              header: 'UI/UX Engineer',
-              body: <Experience {...composecure} />,
+      <div className="h-full max-h-3/5">
+        <Tabs
+          defaultTab={0}
+          tabs={[
+            {
+              label: 'composecure',
+              subLabel: composecure.date,
+              value: 'composecure',
+              content: {
+                header: 'UI/UX Engineer',
+                body: <Experience {...composecure} />,
+              },
             },
-          },
-          {
-            label: 'Rutgers - dcs',
-            subLabel: dcs.date,
-            value: 'dcs',
-            content: {
-              header: 'Special Project Manager/Web Developer',
-              body: <Experience {...dcs} />,
+            {
+              label: 'Rutgers - dcs',
+              subLabel: dcs.date,
+              value: 'dcs',
+              content: {
+                header: 'Special Project Manager/Web Developer',
+                body: <Experience {...dcs} />,
+              },
             },
-          },
-          {
-            label: 'Rutgers - OIT',
-            subLabel: oit.date,
-            value: 'oit',
-            content: {
-              header: 'Multimedia Specialist',
-              body: <Experience {...oit} />,
+            {
+              label: 'Rutgers - OIT',
+              subLabel: oit.date,
+              value: 'oit',
+              content: {
+                header: 'Multimedia Specialist',
+                body: <Experience {...oit} />,
+              },
             },
-          },
-        ]}
-      />
+          ]}
+        />
+      </div>
     </div>
   );
 };
