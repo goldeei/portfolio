60876a90f9dcf38a3ec963da90f818bffc135f8c
Jake Goldfarb <jgoldfarb@composecure.com>
2025-02-26 13:55:14 -0500

    Layout

--- src/components/tabs/tab-content.tsx
+++ src/components/tabs/tab-content.tsx
@@ -16,10 +16,10 @@ export const TabContent = ({ ...props }: TabContentProps) => {
         initial={{ y: 10, opacity: 0 }}
         animate={{ y: 0, opacity: 1 }}
         exit={{ y: -10, opacity: 0 }}
-        className="relative overflow-auto py-8 text-secondary"
+        className="text-secondary relative my-2 overflow-auto"
         transition={transitionProps}
       >
-        <h3 className="sticky inset-0 bg-background pb-1 text-2xl font-medium">
+        <h3 className="bg-background sticky inset-0 py-4 text-2xl font-medium">
           {header}
         </h3>
         <div>{children}</div>
