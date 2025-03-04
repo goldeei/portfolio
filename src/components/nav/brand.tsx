19840dcf43a7d01381f31214d7e4fe1d31c8e631
Jake Goldfarb <jgoldfarb@composecure.com>
2025-03-04 11:39:44 -0500

    Pass classname

--- src/components/nav/brand.tsx
+++ src/components/nav/brand.tsx
@@ -1,13 +1,24 @@
 import { Logo } from '@/assets/icons';
 
+import { cn } from '@/lib/style-utils';
 import { Button } from '../button';
 
-export const Brand = () => {
+interface BrandProps {
+  className?: string;
+}
+export const Brand = ({ ...props }: BrandProps) => {
+  const { className } = props;
+
   return (
-    <div className="relative flex size-fit items-center justify-center">
+    <div
+      className={cn(
+        'relative flex size-16 items-center justify-center',
+        className,
+      )}
+    >
       <Button
         variant="link"
-        className="z-10 size-16 border-none p-0 inset-shadow-sm transition-transform hover:-translate-y-0.5 [&_svg]:size-full"
+        className="z-10 size-full border-none p-0 inset-shadow-sm transition-transform hover:-translate-y-0.5 [&_svg]:size-full"
       >
         <Logo />
       </Button>
