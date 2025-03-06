869482d85529feb1ac99b481379ba0dd5ef56fa8
Jake Goldfarb <jgoldfarb@composecure.com>
2025-03-06 11:07:04 -0500

    Refactor

--- src/components/tabs/index.tsx
+++ src/components/tabs/index.tsx
@@ -2,20 +2,20 @@ import { useMemo, useState } from 'react';
 
 import { BREAKPOINT } from '@/constants';
 import { cn } from '@/lib/style-utils';
-import { motion } from 'motion/react';
 import { useWindowSize } from 'usehooks-ts';
-import { ActiveIndicator } from './active-indicator';
 import { TabContent } from './tab-content';
-import { TabLabel } from './tab-label';
+import { TabNav } from './tab-nav';
+
+export type TabItem = {
+  label: string;
+  subLabel?: string;
+  value: string;
+  content: { header: string; body: string | React.ReactNode };
+};
 
 type TabGroupProps = {
-  tabs: {
-    label: string;
-    subLabel?: string;
-    value: string;
-    content: { header: string; body: string | React.ReactNode };
-  }[];
-  onValueChange?: (value: string) => void;
+  tabs: TabItem[];
+  onValueChange?: (idx: number) => void;
   defaultTab?: number;
 } & (
   | {
@@ -41,6 +41,7 @@ export const Tabs = (props: TabGroupProps) => {
     defaultTab,
     orientation = 'vertical',
     autoOrientation = true,
+    onValueChange,
   } = props;
 
   const [currentTabIdx, setCurrentTabIdx] = useState(defaultTab || 0);
@@ -56,58 +57,20 @@ export const Tabs = (props: TabGroupProps) => {
   const handleTabChange = (selectedTabIdx: number) => {
     if (selectedTabIdx === currentTabIdx) return;
 
-    setCurrentTabIdx(selectedTabIdx);
+    if (onValueChange) {
+      onValueChange(selectedTabIdx);
+    }
+    return setCurrentTabIdx(selectedTabIdx);
   };
 
   return (
     <div className={cn('flex h-full gap-8', isVertical && 'flex-col')}>
-      <nav>
-        <ul
-          className={cn(
-            'relative mb-4 flex h-full flex-col',
-            isVertical && 'w-full flex-row justify-start overflow-x-auto pb-2',
-          )}
-        >
-          {tabs.map(({ value, label, subLabel }, idx) => (
-            <motion.li
-              key={value}
-              className={cn(
-                'relative flex cursor-pointer',
-                isVertical
-                  ? 'flex-1 flex-col-reverse justify-between px-6 text-center'
-                  : 'items-center gap-2 py-6',
-              )}
-              onClick={() => handleTabChange(idx)}
-            >
-              <ActiveIndicator
-                value={value}
-                isActive={idx === currentTabIdx}
-                isVertical={isVertical}
-              />
-              <TabLabel
-                label={label}
-                subLabel={subLabel}
-                isActive={idx === currentTabIdx}
-              />
-              <div
-                className={cn(
-                  'absolute -z-10 box-border flex',
-                  isVertical
-                    ? 'right-0 bottom-0 h-5 w-full items-center'
-                    : 'top-0 left-0 h-full w-5 justify-center',
-                )}
-              >
-                <div
-                  className={cn(
-                    'bg-accent absolute',
-                    isVertical ? 'h-1 w-full' : 'h-full w-1',
-                  )}
-                />
-              </div>
-            </motion.li>
-          ))}
-        </ul>
-      </nav>
+      <TabNav
+        isVertical={isVertical}
+        tabs={tabs}
+        currentIdx={currentTabIdx}
+        onTabChange={handleTabChange}
+      />
       <TabContent
         value={tabs[currentTabIdx].value}
         header={tabs[currentTabIdx].content.header}
---
+++ src/components/tabs/tab-nav.tsx
@@ -0,0 +1,66 @@
+import { cn } from '@/lib/style-utils';
+import { motion } from 'motion/react';
+import { TabItem } from '.';
+import { ActiveIndicator } from './active-indicator';
+import { TabLabel } from './tab-label';
+
+interface TabNavProps {
+  isVertical: boolean;
+  tabs: TabItem[];
+  currentIdx: number;
+  onTabChange: (tabIdx: number) => void;
+}
+
+export const TabNav = (props: TabNavProps) => {
+  const { tabs, isVertical, currentIdx, onTabChange } = props;
+
+  return (
+    <nav>
+      <ul
+        className={cn(
+          'relative mb-4 flex h-full flex-col',
+          isVertical && 'w-full flex-row justify-start overflow-x-auto pb-2',
+        )}
+      >
+        {tabs.map(({ value, label, subLabel }, idx) => (
+          <motion.li
+            key={value}
+            className={cn(
+              'relative flex cursor-pointer',
+              isVertical
+                ? 'flex-1 flex-col-reverse justify-between px-6 text-center'
+                : 'items-center gap-2 py-6',
+            )}
+            onClick={() => onTabChange(idx)}
+          >
+            <ActiveIndicator
+              value={value}
+              isActive={idx === currentIdx}
+              isVertical={isVertical}
+            />
+            <TabLabel
+              label={label}
+              subLabel={subLabel}
+              isActive={idx === currentIdx}
+            />
+            <div
+              className={cn(
+                'absolute -z-10 box-border flex',
+                isVertical
+                  ? 'right-0 bottom-0 h-5 w-full items-center'
+                  : 'top-0 left-0 h-full w-5 justify-center',
+              )}
+            >
+              <div
+                className={cn(
+                  'bg-accent absolute',
+                  isVertical ? 'h-1 w-full' : 'h-full w-1',
+                )}
+              />
+            </div>
+          </motion.li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
