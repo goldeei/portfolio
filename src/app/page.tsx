4b13db2bbed7ccd43ee4b2d20cc5acdcc19b6a06
Jake Goldfarb <jgoldfarb@composecure.com>
2026-01-21 15:13:34 -0500

    Refactor page.tsx layout for improved readability and structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
+import { Header } from '@/components';
 
 export default function Home() {
   return (
     <div className="">
+      <Header />
+
       <main className="">
         <div>
-          <p className="text-primary text-6xl font-medium ">Frontend <br/ > Engineer</p>
-          <p className="text-muted text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quasi temporibus ratione dolores nesciunt odit. Quaerat, iste. Dolore, atque. Placeat minus eius molestias quis unde! Suscipit praesentium architecto ratione nobis.</p>
+          <p className="text-primary text-6xl font-medium">
+            Frontend <br /> Engineer
+          </p>
+          <p className="text-muted text-2xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quasi temporibus ratione dolores nesciunt
+            odit. Quaerat, iste. Dolore, atque. Placeat minus eius molestias quis unde! Suscipit praesentium architecto
+            ratione nobis.
+          </p>
         </div>
-        </main>
+      </main>
     </div>
   );
 }
