7a97835bef7bc4154f7b83f0f74a46022802a9e7
Jake Goldfarb <jgoldfarb@composecure.com>
2026-01-22 08:59:10 -0500

    Refactor Header component: introduce HeaderLink for cleaner link rendering and enhance styling for improved navigation experience

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,31 @@
+import { FileUser, Github, Linkedin, Mail } from 'lucide-react';
 import Link from 'next/link';
+
+const HeaderLink = ({ href, icon }: { href: string; icon: React.ReactNode }) => {
+  return (
+    <Link href={href} className='text-text-muted hover:text-secondary transition-colors shadow-2xl hover:shadow-secondary/90'>
+      {icon}
+    </Link>
+  );
+};
 
 export const Header = () => {
   return (
-    <nav className="flex items-center justify-between p-4">
-      <Link href="/">Jake Goldfarb</Link>
-      <ul className="flex gap-4">
+    <nav className="flex items-center justify-between py-9 border-b border-border sticky top-0 bg-background/95 backdrop-blur-sm shadow-2xl px-4"> 
+      <Link href="/" className='text-text-muted hover:text-secondary transition-colors text-xl'>Jake Goldfarb</Link>
+      <ul className="flex gap-9">
         <li>
-          <Link href="#">Resume</Link>
+            <HeaderLink href="#" icon={<FileUser className="size-6" />} />    
         </li>
         <li>
-          <Link href="#">Mail Me</Link>
+          <HeaderLink href="#" icon={<Mail className="size-6" />} />
         </li>
         <li>
-          <Link href="#">GitHub</Link>
+          <HeaderLink href="#" icon={<Github className="size-6" />} />
         </li>
         <li>
-          <Link href="#">LinkedIn</Link>
-        </li>
+          <HeaderLink href="#" icon={<Linkedin className="size-6" />} />
+          </li>
       </ul>
     </nav>
   );
