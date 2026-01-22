aa37adfe1ab29677ea708f43367f321d57bdc206
Jake Goldfarb <jgoldfarb@composecure.com>
2026-01-22 17:02:54 -0500

    style: enhance layout of Header and Home components for improved responsiveness
    Refactor Header component to include a wrapper for better alignment and spacing. Update Home component to ensure proper item alignment on larger screens, enhancing the overall layout and responsiveness of the application.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,12 @@ export default function Home() {
       <Header />
       <main>
 
-        <div className="block sm:flex max-w-7xl mx-auto px-4 sm:pt-(--header-height) gap-8" style={{ height: 'calc(100dvh - var(--header-height))' }}>
+        <div className="block sm:flex sm:items-center max-w-7xl mx-auto px-4 sm:pt-(--header-height) gap-8" style={{ height: 'calc(100dvh - var(--header-height))' }}>
           <aside className='hidden sm:block'>
             <SideNav />
           </aside> 
           <HomePage className='sm:hidden' />
-          <div>
+          <div >
             <div>
               Place holder page with stuff and things Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi sit, dolor officia pariatur, nisi eaque praesentium mollitia, dolorum quisquam harum iste quasi! Distinctio cupiditate beatae id provident totam eos excepturi.
             </div>
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,9 +3,11 @@ import { MobileHeader } from "./MobileHeader";
 
 export const Header = () => {
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 border-b border-border bg-background/95 backdrop-blur-sm shadow-2xl flex items-center justify-between px-4 py-6">
+    <nav className="fixed top-0 left-0 right-0 z-50">
+      <div className="max-w-7xl mx-auto px-4 flex items-center justify-between  border-b border-border bg-background/95 backdrop-blur-sm shadow-2xl py-6">
         <DesktopHeader className="hidden sm:flex w-full items-center justify-between"/>
         <MobileHeader className="flex w-full items-center justify-between sm:hidden" />
+      </div>
     </nav>
   );
 };
