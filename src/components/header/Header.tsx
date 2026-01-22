14542bebc1450fa5535eaf08e755f1b5d355d095
Jake Goldfarb <jgoldfarb@composecure.com>
2026-01-22 09:48:44 -0500

    Update Header component for responsive design: change breakpoint from md to sm for DesktopHeader and MobileHeader visibility

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,8 +4,8 @@ import { MobileHeader } from "./MobileHeader";
 export const Header = () => {
   return (
     <nav className="border-b border-border sticky top-0 bg-background/95 backdrop-blur-sm shadow-2xl px-4 py-9">
-        <DesktopHeader className="hidden md:flex  items-center justify-between"/>
-        <MobileHeader className="flex items-center justify-between md:hidden" />
+        <DesktopHeader className="hidden sm:flex  items-center justify-between"/>
+        <MobileHeader className="flex items-center justify-between sm:hidden" />
     </nav>
   );
 };
