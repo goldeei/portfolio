235a8735f08a6e9204c62f8e62ab5830e914b62c
Jake Goldfarb <jgoldfarb@composecure.com>
2026-01-29 10:44:56 -0500

    refactor: update page layout to integrate Hero component and improve structure
    Replace MobileHero with a new Hero component and restructure the main layout to enhance responsiveness. Adjust grid layout for better section organization and visibility of SideNav on larger screens.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
-import { Experience, Header, MobileHero } from '@/components';
+import { Experience, Header } from '@/components';
+import { Hero } from '@/components/hero';
 import { AboutMe } from '@/components/sections/about-me';
 import { LetsConnect } from '@/components/sections/lets-connect';
 import { SideNav } from '@/components/side-nav';
@@ -7,20 +8,18 @@ export default function Home() {
   return (
     <>
       <Header />
-      <main>
-        <div
-          className="mx-auto grid max-w-7xl grid-cols-1 gap-8 px-4 sm:mt-(--header-height) sm:grid-cols-[300px_auto] sm:gap-24"
-          style={{ height: 'calc(100dvh - var(--header-height))' }}
-        >
-          <SideNav />
-          <div className="sm:max-h-svh sm:overflow-y-auto">
-            <MobileHero className="sm:hidden" />
-            <AboutMe />
-            <Experience />
-            <LetsConnect />
-          </div>
-        </div>
-      </main>
+      <div
+        className="mt-(--header-height) grid grid-cols-1 sm:grid-cols-[300px_auto] sm:grid-rows-2"
+        style={{ height: 'calc(100dvh - var(--header-height))' }}
+      >
+        <Hero />
+        <main className="sm:row-span-2 sm:max-h-svh sm:overflow-y-auto">
+          <AboutMe />
+          <Experience />
+          <LetsConnect />
+        </main>
+        <SideNav className="hidden sm:col-start-1 sm:row-start-2 sm:block" />
+      </div>
     </>
   );
 }
