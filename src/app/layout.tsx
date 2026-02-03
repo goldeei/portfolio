44b8b9387f3be9bdb2661aaff6fb097fd27cc2d0
Jake Goldfarb <jgoldfarb@composecure.com>
2026-02-03 11:49:49 -0500

    feat: update font configuration and styling in layout and globals
    - Replace Geist font with Figtree for improved typography
    - Define new font variables for sans and mono in globals.css
    - Update body styling to utilize the new font settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,15 @@
 import { Background } from '@/components';
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Figtree, Geist_Mono } from 'next/font/google';
 import './globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
 
 const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+const figtree = Figtree({
+  variable: '--font-figtree',
   subsets: ['latin'],
 });
 
@@ -25,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${geistMono.variable} ${figtree.variable} bg-background text-foreground antialiased`}>
         <Background />
         {children}
       </body>
