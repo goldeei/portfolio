cdbbf160486a4f9a2a6d2f88199e54794b1e3983
Jake Goldfarb <jgoldfarb@composecure.com>
2026-02-10 14:54:58 -0500

    chore: remove unused IconButton component
    Component is no longer needed after migrating to Button with asChild
    pattern. All icon button functionality now handled by Button component.

--- src/components/icon-button.tsx
+++
@@ -1,9 +0,0 @@
-import { cn } from "@/lib/utils"
-
-export const IconButton = ({className, icon, ...props}: {className?: string, icon: React.ReactNode} & React.ButtonHTMLAttributes<HTMLButtonElement>) => {
-    return (
-        <button className={cn("interactive-element p-1.5", className)} {...props}>
-            {icon}
-        </button>
-    )
-}
