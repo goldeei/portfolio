a17320baf8a119d3f11408e51b3a0b0362634b6c
Jake Goldfarb <jgoldfarb@composecure.com>
2026-02-16 13:38:01 -0500

    Add basic parallax

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,14 +1,28 @@
 'use client';
 
-import { motion } from 'framer-motion';
+import { useIsMobile } from '@/hooks';
+import { getScrollContainer } from '@/lib/scroll-utils';
+import { motion, useScroll, useTransform } from 'framer-motion';
+import { useRef } from 'react';
 
 export const Background = () => {
+  const isMobile = useIsMobile();
+  const scrollContainer = useRef<HTMLElement>(getScrollContainer(isMobile));
+
+  const { scrollY } = useScroll({
+    container: scrollContainer,
+  });
+  const y = useTransform(scrollY, (y) => y * -0.1);
+
   return (
     <motion.div
       style={{
         background: 'var(--gradient-bg)',
+        y,
+        top: '-25vh',
+        bottom: '-25vh',
       }}
-      className="fixed inset-0 -z-10"
+      className="fixed right-0 left-0 -z-10"
     />
   );
 };
