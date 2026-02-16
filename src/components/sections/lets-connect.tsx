0b961b78ec2a898862dd8201dd7388cefb56cd82
Jake Goldfarb <jgoldfarb@composecure.com>
2026-02-16 13:05:01 -0500

    refactor: refine let's connect section blurb

--- src/components/sections/lets-connect.tsx
+++ src/components/sections/lets-connect.tsx
@@ -5,9 +5,9 @@ import { Typography } from '../typography';
 
 const BlurbSection = () => {
   return (
-    <Typography className="mb-4 max-w-xl text-center leading-loose text-balance">
-      I'm looking for frontend engineering work where I can solve real problems and learn new things. If you're building
-      something interesting, let's see where I might fit in.
+    <Typography className="mb-4 max-w-xl text-center text-balance">
+      I'm looking for frontend engineering work where I can solve real problems, feel my impact, and learn new things.
+      If you're building something interesting, let's see where I might fit in.
     </Typography>
   );
 };
