fddb41c86f338644ddca02b70d636dfd2a20aca0
Jake Goldfarb <jgoldfarb@composecure.com>
2026-02-19 09:41:13 -0500

    feat(body-portal): add BodyPortal component for rendering outside main DOM tree

---
+++ src/components/body-portal.tsx
@@ -0,0 +1,11 @@
+import { createPortal } from 'react-dom';
+
+/**
+ * Wrapper for creating a portal to the body element.
+ * This is used to render components outside of the main DOM tree.
+ * @param children - The children to render.
+ * @returns A portal to the body element.
+ */
+export const BodyPortal = ({ children }: { children: React.ReactNode }) => {
+  return createPortal(children, document.body);
+};
