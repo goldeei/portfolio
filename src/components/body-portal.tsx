e9308dba5f24565273163510b2f88b6ef327ab4d
Jake Goldfarb <jgoldfarb@composecure.com>
2026-02-20 08:44:14 -0500

    fix(body-portal): prevent rendering on the server side and set display name

--- src/components/body-portal.tsx
+++ src/components/body-portal.tsx
@@ -7,5 +7,8 @@ import { createPortal } from 'react-dom';
  * @returns A portal to the body element.
  */
 export const BodyPortal = ({ children }: { children: React.ReactNode }) => {
+  if (typeof window === 'undefined') return null;
   return createPortal(children, document.body);
 };
+
+BodyPortal.displayName = 'BodyPortal';
